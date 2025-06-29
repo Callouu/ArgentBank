6b818683721a549a76cf893abbe6ea8f5ccbc784
Alexis Bouvry <bouvryalexis@gmail.com>
2025-06-29 21:09:21 +0200

    change routes path and add footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
+
+/**
+ * Render a footer element with a paragraph element containing the text 'Copyright
+ * 2020 Argent Bank'.
+ *
+ * @category Components
+ * @component
+ * @returns { React.Component } A React component
+ */
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer className="footer">
+			<p className="footer-text">Copyright 2020 Argent Bank</p>
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
