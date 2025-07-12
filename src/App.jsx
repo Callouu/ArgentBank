4eb9921bf3d575df1d85e6a1890cbd4c8e2ab7cf
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-12 11:23:21 +0200

    add storage token to save user after refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { useDispatch } from 'react-redux'
+import { fetchUserProfile } from './store/userslice'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(fetchUserProfile());
+    }
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
-				<Header />
-				<Routes>
-					<Route exact path="/" element={<Home />} />
-					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/profile" element={<Profile />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
-  )
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
