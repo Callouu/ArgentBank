11fdfd0f16a6ed55b02bce31168f002531eb1996
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-12 11:28:02 +0200

    fix homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,7 +16,6 @@ function Home() {
 		<main>
 			<div className="hero">
 				<section className="hero-content">
-					<h2 className="sr-only">Promoted Content</h2>
 					<p className="subtitle">No fees.</p>
 					<p className="subtitle">No minimum deposit.</p>
 					<p className="subtitle">High interest rates.</p>
@@ -24,7 +23,6 @@ function Home() {
 				</section>
 			</div>
 			<section className="features">
-				<h2 className="sr-only">Features</h2>
 				<Feature img={iconChat} title="You are our #1 priority" text="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
 				<Feature img={iconMoney} title="More savings means higher rates" text="The more you save with us, the higher your interest rate will be!" />
 				<Feature img={iconSecurity} title="Security you can trust" text="We use top of the line encryption to make sure your data and money is always safe." />
