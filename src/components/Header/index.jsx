1abb45f2c70e53e211b3ea10d9863dd14b390ae4
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-12 11:26:55 +0200

    fix header components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,33 +2,39 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router'
 import Logo from "../../assets/argentBankLogo.png";
-// import your logo image
-// import { logout } from '../../store/userSlice' // Uncomment if you have a logout action
+import { logout } from '../../store/userslice'
 
 function Header() {
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const user = useSelector((state) => state.user)
   const isLoggedIn = user && user.isAuthenticated
 
   const handleSignOut = () => {
-    // dispatch(logout()) // Uncomment if you have a logout action
+    dispatch(logout())
   }
 
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
-        {/* Replace with your logo image */}
         <img src={Logo} alt="Argent Bank Logo" />
       </Link>
       <div>
         {isLoggedIn ? (
           <>
-            <span className="main-nav-item">
-              <i className="fa fa-user-circle"></i> {user.firstName}
-            </span>
-            <button className="main-nav-item" onClick={handleSignOut}>
+          <Link className="main-nav-item" to="/profile">
+						<i className="fa fa-user-circle"></i>
+						{user.profile?.firstName}
+					</Link>
+            {/* <span className="main-nav-item">
+              <i className="fa fa-user-circle"></i> {user.profile?.firstName}
+            </span> */}
+            {/* <button className="main-nav-item" onClick={handleSignOut}>
               <i className="fa fa-sign-out"></i> Sign Out
-            </button>
+            </button> */}
+            <Link onClick={handleSignOut} className="main-nav-item" to="/">
+						<i className="fa fa-sign-out"></i>
+						Sign Out
+					</Link>
           </>
         ) : (
           <Link className="main-nav-item" to="/login">
