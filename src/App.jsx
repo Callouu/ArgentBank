2f92f51e3240e41efcc7f5cb290c21dd9b0fc655
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-13 11:41:57 +0200

    clean files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { useDispatch } from 'react-redux'
-import { fetchUserProfile, initializeToken } from './store/userslice'
+import { fetchUserProfile} from './store/userslice'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
