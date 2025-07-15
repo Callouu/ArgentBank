e070c34cd7562d457b028677294aefefd9effdc7
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-15 15:46:13 +0200

    remove session storage token after refresh

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
+window.addEventListener("beforeunload", () => {
+  sessionStorage.removeItem("token");
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
