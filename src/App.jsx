b7f3dc9b989f4b874c9abe7f33860ba0890a7f4d
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-15 22:44:55 +0200

    fix url import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { useDispatch } from 'react-redux'
-import { fetchUserProfile} from './store/userslice'
+import { fetchUserProfile} from './store/userSlice'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
